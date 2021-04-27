07c7ade1d5d20a73afbc3dac0ea80ae5578d2060
Renan Pupin <renan.pupin@gmail.com>
2021-04-27 09:44:14 -0300

    feature: start color scale

---
+++ src/libs/colors.js
@@ -0,0 +1,57 @@
+export const colorScale = [
+    "#E53935",
+    "#43A047",
+    "#1E88E5",
+    "#D81B60",
+    "#FDD835",
+    "#3949AB",
+    "#8E24AA",
+    "#00897B",
+    "#039BE5",
+    "#5E35B1",
+    "#7CB342",
+    "#F4511E",
+    "#757575",
+    "#C0CA33",
+    "#00ACC1",
+    "#6D4C41",
+    "#FFB300",
+    "#546E7A",
+    "#FB8C00"
+]
+
+export const colorScaleUnsorted = [
+    "#E53935",
+    "#D81B60",
+    "#8E24AA",
+    "#5E35B1",
+    "#3949AB",
+    "#1E88E5",
+    "#039BE5",
+    "#00ACC1",
+    "#00897B",
+    "#43A047",
+    "#7CB342",
+    "#C0CA33",
+    "#FDD835",
+    "#FFB300",
+    "#FB8C00",
+    "#F4511E",
+    "#6D4C41",
+    "#757575",
+    "#546E7A"
+]
+
+export const colorScaleRandom = [
+    "#a6cee3","#1f78b4","#b2df8a","#33a02c",
+    "#fb9a99","#e31a1c","#fdbf6f","#ff7f00",
+    "#cab2d6","#6a3d9a","#ffff99","#b15928"
+]
+
+export const colorScaleRandom2 = [
+    "#1f78b4",
+    "#fdbf6f",
+    "#ff7f00",
+    "#6a3d9a",
+    "#e31a1c"
+]
