465a261f0a501d9236a068ac3cdedd721ce21c42
Renan Pupin <renan.pupin@gmail.com>
2024-06-09 21:47:43 +0200

    ops: clean

--- src/components/Map/HTMLMarker/HTMLMarker.tsx
+++ src/components/Map/HTMLMarker/HTMLMarker.tsx
@@ -1,74 +1,71 @@
 /* global google */
 
-const createHTMLMapMarker = ({
-    OverlayView = google.maps.OverlayView,
-    ...args
-}) => {
+const createHTMLMapMarker = ({OverlayView = google.maps.OverlayView, ...args}) => {
     class HTMLMapMarker extends OverlayView {
-        private latlng: any;
-        private html: any;
-        private div: any;
+        private latlng: any
+        private html: any
+        private div: any
         constructor() {
-            super();
-            this.latlng = args.latlng;
-            this.html = args.html;
-            this.setMap(args.map);
+            super()
+            this.latlng = args.latlng
+            this.html = args.html
+            this.setMap(args.map)
         }
 
         createDiv() {
-            this.div = document.createElement("div");
-            this.div.style.position = "absolute";
+            this.div = document.createElement('div')
+            this.div.style.position = 'absolute'
             if (this.html) {
-                this.div.innerHTML = this.html;
+                this.div.innerHTML = this.html
             }
-            google.maps.event.addDomListener(this.div, "click", event => {
-                google.maps.event.trigger(this, "click");
-            });
+            google.maps.event.addDomListener(this.div, 'click', () => {
+                google.maps.event.trigger(this, 'click')
+            })
         }
 
         appendDivToOverlay() {
-            const panes = this.getPanes();
+            const panes = this.getPanes()
             // @ts-ignore
-            panes.overlayImage.appendChild(this.div);
+            panes.overlayImage.appendChild(this.div)
         }
 
         positionDiv() {
-            const point = this.getProjection().fromLatLngToDivPixel(this.latlng);
-            let offset = 25;
+            const point = this.getProjection().fromLatLngToDivPixel(this.latlng)
+            let offset = 25
             if (point) {
-                this.div.style.left = `${point.x - offset}px`;
-                this.div.style.top = `${point.y - offset}px`;
+                this.div.style.left = `${point.x - offset}px`
+                this.div.style.top = `${point.y - offset}px`
             }
         }
 
         draw() {
             if (!this.div) {
-                this.createDiv();
-                this.appendDivToOverlay();
+                this.createDiv()
+                this.appendDivToOverlay()
             }
-            this.positionDiv();
+            this.positionDiv()
         }
 
         remove() {
             if (this.div) {
-                this.div.parentNode.removeChild(this.div);
-                this.div = null;
+                this.div.parentNode.removeChild(this.div)
+                this.div = null
             }
         }
 
         getPosition() {
-            return this.latlng;
+            return this.latlng
         }
 
         getDraggable() {
-            return false;
+            return false
         }
     }
 
-    return new HTMLMapMarker();
-};
+    return new HTMLMapMarker()
+}
 
-export default createHTMLMapMarker;
+export default createHTMLMapMarker
 
 // let marker = createHTMLMapMarker({
 //     latlng: latLng,
