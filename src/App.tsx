af26da65269fe96717e7273e520abcbc294aa112
Renan Pupin <renan.pupin@gmail.com>
2024-06-09 22:00:11 +0200

    ops: fix initial value

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,22 @@
-import React, {useState} from 'react';
-import {useSelector} from "react-redux";
+import React, {useState} from 'react'
+import {useSelector} from 'react-redux'
 import Map from 'src/components/Map/Map'
 import Table from 'src/components/Table/Table'
 import Menu from 'src/components/Menu/Menu'
 import Chart from 'src/components/Chart/Chart'
 import ParallelCoordinates from 'src/components/ParallelCoordinates/ParallelCoordinates'
-import FilterList from "src/components/FilterList/FilterList";
-import TemporalFilter from "src/components/TemporalFilter/TemporalFilter";
-import LayersList from "src/components/LayersList/LayersList";
-import SideMenu from "src/components/SideMenu/SideMenu";
-import {getTemporalAttribute, getVisualizations} from "src/redux/data/selectors";
+import FilterList from 'src/components/FilterList/FilterList'
+import TemporalFilter from 'src/components/TemporalFilter/TemporalFilter'
+import LayersList from 'src/components/LayersList/LayersList'
+import SideMenu from 'src/components/SideMenu/SideMenu'
+import {getTemporalAttribute, getVisualizations} from 'src/redux/data/selectors'
 import {VisualizationTypeValues} from 'src/redux/data/types'
 
 const App: React.FC = () => {
-    const [viewMode, setViewMode] = useState<'map' | 'table'>('map');
-    const [visMode, setVisMode] = useState<'split' | 'full'>('full');
-    const temporalAttribute = useSelector(getTemporalAttribute);
-    const visualizations = useSelector(getVisualizations);
+    const [viewMode, setViewMode] = useState<'map' | 'table'>('map')
+    const [visMode, setVisMode] = useState<'split' | 'full'>('split')
+    const temporalAttribute = useSelector(getTemporalAttribute)
+    const visualizations = useSelector(getVisualizations)
     // console.log('visualizations', visualizations)
     // console.log('app temporalAttribute', !!temporalAttribute)
     return (
@@ -29,16 +29,42 @@ const App: React.FC = () => {
             />
 
             <div className={'splitView'} style={{display: 'flex', flexDirection: 'row'}}>
-                <div className={'dataViewWrapper'} style={{flex: 1, width: visMode === 'split' ? `min(80%, 80% - 400px)` : '100%'}}>
-                    {viewMode === 'map' ? <Map/> : <Table/>}
+                <div
+                    className={'dataViewWrapper'}
+                    style={{flex: 1, width: visMode === 'split' ? `min(80%, 80% - 400px)` : '100%'}}
+                >
+                    {viewMode === 'map' ? <Map /> : <Table />}
                 </div>
-                <div className={'chartsWrapper'} style={{flex: 0, flexDirection: 'column', maxHeight: `calc(100vh - 65px)`, marginTop: 65, overflowY: 'scroll', width: visMode === 'split' ? `max(20%, 400px)` : '0%', display: 'flex', minWidth: visMode === 'split' ? 400 : 0}}>
-                    {visualizations.filter((item: any) => item.visible && item.type === VisualizationTypeValues.Chart).map((item: any, index: number) => (
-                        <Chart key={item.id} index={index} visData={item} visMode={visMode}/>
-                    ))}
-                    {visualizations.filter((item: any) => item.visible && item.type === VisualizationTypeValues.ParallelCoordinates).map((item: any, index: number) => (
-                        <ParallelCoordinates key={index} visMode={visMode}/>
-                    ))}
+                <div
+                    className={'chartsWrapper'}
+                    style={{
+                        flex: 0,
+                        flexDirection: 'column',
+                        maxHeight: `calc(100vh - 65px)`,
+                        marginTop: 65,
+                        overflowY: 'scroll',
+                        width: visMode === 'split' ? `max(20%, 400px)` : '0%',
+                        display: 'flex',
+                        minWidth: visMode === 'split' ? 400 : 0
+                    }}
+                >
+                    {visualizations
+                        .filter(
+                            (item: any) =>
+                                item.visible && item.type === VisualizationTypeValues.Chart
+                        )
+                        .map((item: any, index: number) => (
+                            <Chart key={item.id} index={index} visData={item} visMode={visMode} />
+                        ))}
+                    {visualizations
+                        .filter(
+                            (item: any) =>
+                                item.visible &&
+                                item.type === VisualizationTypeValues.ParallelCoordinates
+                        )
+                        .map((item: any, index: number) => (
+                            <ParallelCoordinates key={index} visMode={visMode} />
+                        ))}
                 </div>
             </div>
 
@@ -46,14 +72,14 @@ const App: React.FC = () => {
                 items={[
                     {
                         title: 'Layers',
-                        component: <LayersList/>,
+                        component: <LayersList />,
                         icon: 'layers'
                     },
                     {
                         title: 'Filters',
-                        component: <FilterList/>,
+                        component: <FilterList />,
                         icon: 'filter_alt'
-                    },
+                    }
                 ]}
             />
 
@@ -76,30 +102,34 @@ const App: React.FC = () => {
                     setVisMode(visMode === 'split' ? 'full' : 'split')
                 }}
             >
-                <i className="material-icons">{visMode === 'split' ? 'fullscreen' : 'vertical_split'}</i>
+                <i className="material-icons">
+                    {visMode === 'split' ? 'fullscreen' : 'vertical_split'}
+                </i>
             </div>
 
-            {!!temporalAttribute && viewMode === 'map' && <div
-                style={{
-                    position: 'absolute',
-                    bottom: 10,
-                    left: 'calc(50% - 250px)',
-                    width: 500,
-                    // height: 75,
-                    padding: 20,
-                    backgroundColor: '#fff',
-                    display: 'flex',
-                    alignItems: 'center',
-                    justifyContent: 'center',
-                    cursor: 'pointer',
-                    borderRadius: 6,
-                    userSelect: 'none'
-                }}
-            >
-                <TemporalFilter/>
-            </div>}
+            {!!temporalAttribute && viewMode === 'map' && (
+                <div
+                    style={{
+                        position: 'absolute',
+                        bottom: 10,
+                        left: 'calc(50% - 250px)',
+                        width: 500,
+                        // height: 75,
+                        padding: 20,
+                        backgroundColor: '#fff',
+                        display: 'flex',
+                        alignItems: 'center',
+                        justifyContent: 'center',
+                        cursor: 'pointer',
+                        borderRadius: 6,
+                        userSelect: 'none'
+                    }}
+                >
+                    <TemporalFilter />
+                </div>
+            )}
         </div>
-    );
+    )
 }
 
-export default App;
+export default App
