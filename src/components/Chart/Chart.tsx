3e585ecc280922d5986cf9228234afdf572ee479
Renan Pupin <renan.pupin@gmail.com>
2024-06-09 17:05:30 +0200

    ops: improve code

--- src/components/Chart/Chart.tsx
+++ src/components/Chart/Chart.tsx
@@ -1,15 +1,18 @@
-import React from 'react';
-import {useSelector, useDispatch} from "react-redux";
-import GoogleCharts from "react-google-charts";
-import {getAttributes, getVisibleRows} from "src/redux/data/selectors";
+import React from 'react'
+import {useSelector, useDispatch} from 'react-redux'
+import GoogleCharts from 'react-google-charts'
+import {getAttributes, getVisibleRows} from 'src/redux/data/selectors'
 
-import ParallelCoordinates from "src/components/ParallelCoordinates/ParallelCoordinates";
-import Draggable from "src/components/Draggable/Draggable";
-import styles from './Chart.module.scss';
+import ParallelCoordinates from 'src/components/ParallelCoordinates/ParallelCoordinates'
+import Draggable from 'src/components/Draggable/Draggable'
+import styles from './Chart.module.scss'
 import {countAttributeOcurrences} from 'src/redux/data/filters'
-import {setHighlight} from "../../redux/data/actions";
+import {setHighlight} from '../../redux/data/actions'
 
 // https://developers.google.com/chart/interactive/docs/printing
+// convert chart to image
+// https://jsfiddle.net/asgallant/R8A8P/1/
+// https://developers.google.com/chart/interactive/docs/printing?hl=pt-br
 
 export type ChartPropTypes = {
     visData: any
@@ -17,10 +20,10 @@ export type ChartPropTypes = {
     index: number
 }
 
-const Chart: React.FC<ChartPropTypes> = (props) => {
+const Chart: React.FC<ChartPropTypes> = props => {
     // console.log("props", props)
-    const { visData, visMode } = props
-    const dispatch = useDispatch();
+    const {visData, visMode} = props
+    const dispatch = useDispatch()
     const visibleRows = useSelector(getVisibleRows)
     const attributes = useSelector(getAttributes)
     // console.log("visData", visData)
@@ -34,75 +37,73 @@ const Chart: React.FC<ChartPropTypes> = (props) => {
     // }
     //hAxis = <field> or id, vAxis = ['numeric1', numeric2]
 
-    const labelAttributeIndex = attributes.findIndex(item => item.name === visData.chartLabelAttribute)
+    const labelAttributeIndex = attributes.findIndex(
+        item => item.name === visData.chartLabelAttribute
+    )
     const attributeIndexX = attributes.findIndex(item => item.name === visData.chartAttributeX)
     const attributeIndexY = attributes.findIndex(item => item.name === visData.chartAttributeY)
 
     const getDataByChartType = () => {
-        if(visData.chartType === 'pie' && visData.hasToGroup){
-            const ocurrences = countAttributeOcurrences(visibleRows, labelAttributeIndex);
-            return ocurrences[0].map((item: any, index: any) => (
-                [
-                    item,
-                    ocurrences[1][index]
-                ]
-            ))
-        }else{
-            return visibleRows.map((item: any, index: any) => (
-                    [
-                        labelAttributeIndex !== -1 ? item[labelAttributeIndex] : item[attributeIndexX],
-                        item[attributeIndexY]
-                    ]
-                ))
-            }
+        if (visData.chartType === 'pie' && visData.hasToGroup) {
+            const ocurrences = countAttributeOcurrences(visibleRows, labelAttributeIndex)
+            return ocurrences[0].map((item: any, index: any) => [item, ocurrences[1][index]])
+        } else {
+            return visibleRows.map((item: any, index: any) => [
+                labelAttributeIndex !== -1 ? item[labelAttributeIndex] : item[attributeIndexX],
+                item[attributeIndexY]
+            ])
+        }
     }
 
     let data = [
-        [visData.chartLabelAttribute || visData.chartAttributeX, visData.chartAttributeY ?? "Ocurrences"],
+        [
+            visData.chartLabelAttribute || visData.chartAttributeX,
+            visData.chartAttributeY ?? 'Ocurrences'
+        ],
         ...getDataByChartType()
     ]
     // console.log("chart data", data)
 
     const getNormalizedChartType = () => {
-        switch (visData.chartType){
-            case "line": {
-                return "LineChart";
+        switch (visData.chartType) {
+            case 'line': {
+                return 'LineChart'
             }
-            case "column": {
-                return "ColumnChart";
+            case 'column': {
+                return 'ColumnChart'
             }
-            case "area": {
-                return "AreaChart";
+            case 'area': {
+                return 'AreaChart'
             }
-            case "histogram": {
-                return "Histogram";
+            case 'histogram': {
+                return 'Histogram'
             }
-            case "pie": {
-                return "PieChart";
+            case 'pie': {
+                return 'PieChart'
             }
-            case "scatter": {
-                return "ScatterChart";
+            case 'scatter': {
+                return 'ScatterChart'
             }
             default: {
-                return visData.chartType;
+                return visData.chartType
             }
         }
     }
 
     const normalizedChartType = getNormalizedChartType()
 
-    const size = 400-35;    // 35 is scrollbar width
-    return(
+    const size = 400 - 35 // 35 is scrollbar width
+    return (
         <Draggable className={styles.chart} disabled={visMode === 'split'}>
             <GoogleCharts
                 width={size}
-                height={size*0.75}
+                height={size * 0.75}
                 chartType={normalizedChartType}
                 loader={<div>Loading Chart</div>}
                 data={data}
                 options={{
                     title: props.visData.title ?? `Chart ${props.index}`,
-                    chartArea: { width: '50%' },
+                    chartArea: {width: '50%'}
                     // hAxis: {
                     //     title: 'Total Population',
                     //     minValue: 0,
@@ -114,23 +115,33 @@ const Chart: React.FC<ChartPropTypes> = (props) => {
                 chartEvents={[
                     {
                         eventName: 'select',
-                        callback: ({ chartWrapper, google }) => {
-                            const chart = chartWrapper.getChart();
+                        callback: ({chartWrapper, google}) => {
+                            const chart = chartWrapper.getChart()
                             // console.log('chart getSelection', chart.getSelection())
                             //@ts-ignore
-                            const selectedDataRow = chart.getSelection()?.length > 0 ? data[chart.getSelection()?.[0]?.row +1] : null
+                            const selectedDataRow =
+                                chart.getSelection()?.length > 0
+                                    ? data[chart.getSelection()?.[0]?.row + 1]
+                                    : null
                             // console.log('selectedDataRow', selectedDataRow)
                             // console.log('visibleRows', visibleRows)
                             // console.log('labelAttributeIndex', labelAttributeIndex)
                             // console.log('attributeIndexX', attributeIndexX)
-                            const highlightIndexes = visibleRows.map((row: any, index: number) => {
-                                const rowAttributeIndex = labelAttributeIndex !== -1 ? labelAttributeIndex : attributeIndexX;
-                                // console.log('selectedDataRow?.[0]', selectedDataRow?.[0])
-                                // console.log('rowAttributeIndex', rowAttributeIndex)
-                                // console.log('row[rowAttributeIndex] value', row[rowAttributeIndex])
-                                // console.log('row[rowAttributeIndex] === selectedDataRow?.[0]', row[rowAttributeIndex], '===', selectedDataRow?.[0], row[rowAttributeIndex] === selectedDataRow?.[0])
-                                return row[rowAttributeIndex] === selectedDataRow?.[0] ? index : null
-                            }).filter((item: any) => item !== null)
+                            const highlightIndexes = visibleRows
+                                .map((row: any, index: number) => {
+                                    const rowAttributeIndex =
+                                        labelAttributeIndex !== -1
+                                            ? labelAttributeIndex
+                                            : attributeIndexX
+                                    // console.log('selectedDataRow?.[0]', selectedDataRow?.[0])
+                                    // console.log('rowAttributeIndex', rowAttributeIndex)
+                                    // console.log('row[rowAttributeIndex] value', row[rowAttributeIndex])
+                                    // console.log('row[rowAttributeIndex] === selectedDataRow?.[0]', row[rowAttributeIndex], '===', selectedDataRow?.[0], row[rowAttributeIndex] === selectedDataRow?.[0])
+                                    return row[rowAttributeIndex] === selectedDataRow?.[0]
+                                        ? index
+                                        : null
+                                })
+                                .filter((item: any) => item !== null)
                             // console.log('highlightIndexes', highlightIndexes)
 
                             dispatch(setHighlight(highlightIndexes as number[]))
--- src/components/Map/InfoWindow/infoWindowUtils.tsx
+++ src/components/Map/InfoWindow/infoWindowUtils.tsx
@@ -1,29 +1,41 @@
-import React from 'react';
-import {render} from "react-dom";
+import React from 'react'
+import {render} from 'react-dom'
 import InfoWindow from './InfoWindow'
 
-export const createInfoWindow = (gmapMarker: any, position: any, title: any, rows: any, map: any, attributes: any, isCluster: boolean, onClose: any) => {
+export const createInfoWindow = (
+    gmapMarker: any,
+    position: any,
+    title: any,
+    rows: any,
+    map: any,
+    attributes: any,
+    isCluster: boolean,
+    onClose: any
+) => {
     const id: string = `info-window-${title}`
-    console.log('createInfoWindow', title, id);
+    // console.log('createInfoWindow', title, id);
 
     const infoWindow = new window.google.maps.InfoWindow({
         content: `<div id="${id}" />`,
         // position: position,
         position: position ? {lat: position.lat(), lng: position.lng()} : undefined,
         pixelOffset: new google.maps.Size(0, isCluster ? -25 : 0)
-    });
+    })
 
     infoWindow.addListener('domready', (e: any) => {
-        render(<InfoWindow title={title} rows={rows} attributes={attributes}/>, document.getElementById(id))
-    });
+        render(
+            <InfoWindow title={title} rows={rows} attributes={attributes} />,
+            document.getElementById(id)
+        )
+    })
 
-    google.maps.event.addListener(infoWindow,'closeclick',function(){
+    google.maps.event.addListener(infoWindow, 'closeclick', function () {
         onClose?.()
-    });
+    })
 
-    infoWindow.open(map, gmapMarker ? gmapMarker : undefined)   //we can omit the marker and set the position internally
+    infoWindow.open(map, gmapMarker ? gmapMarker : undefined) //we can omit the marker and set the position internally
 
-    return infoWindow;
+    return infoWindow
 }
 
 // export const createClusterInfoWindow = (gmapMarkers: any, rows: any, map: any, attributes: any, onClose: any) => {
