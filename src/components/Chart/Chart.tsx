9b5fcce7da25ad290cbeae5de40883af39295a0a
Renan Pupin <renan.pupin@gmail.com>
2024-06-11 11:47:49 +0200

    ops: work on chart

--- src/components/Chart/Chart.tsx
+++ src/components/Chart/Chart.tsx
@@ -1,9 +1,8 @@
-import React from 'react'
+import React, {useCallback, useMemo} from 'react'
 import {useSelector, useDispatch} from 'react-redux'
 import GoogleCharts from 'react-google-charts'
 import {getAttributes, getVisibleRows} from 'src/redux/data/selectors'
 
-import ParallelCoordinates from 'src/components/ParallelCoordinates/ParallelCoordinates'
 import Draggable from 'src/components/Draggable/Draggable'
 import styles from './Chart.module.scss'
 import {countAttributeOcurrences} from 'src/redux/data/filters'
@@ -14,6 +13,8 @@ import {setHighlight} from '../../redux/data/actions'
 // https://jsfiddle.net/asgallant/R8A8P/1/
 // https://developers.google.com/chart/interactive/docs/printing?hl=pt-br
 
+const chartSize = 400 - 35 // 35 is scrollbar width
+
 export type ChartPropTypes = {
     visData: any
     visMode?: any
@@ -21,21 +22,10 @@ export type ChartPropTypes = {
 }
 
 const Chart: React.FC<ChartPropTypes> = props => {
-    // console.log("props", props)
     const {visData, visMode} = props
     const dispatch = useDispatch()
     const visibleRows = useSelector(getVisibleRows)
     const attributes = useSelector(getAttributes)
-    // console.log("visData", visData)
-    // console.log("visibleRows", visibleRows)
-    // console.log("attributes", attributes)
-
-    // chart = {
-    //     title: string
-    //     field: <field> or id
-    //     series: ['numeric1', numeric2]
-    // }
-    //hAxis = <field> or id, vAxis = ['numeric1', numeric2]
 
     const labelAttributeIndex = attributes.findIndex(
         item => item.name === visData.chartLabelAttribute
@@ -43,7 +33,7 @@ const Chart: React.FC<ChartPropTypes> = props => {
     const attributeIndexX = attributes.findIndex(item => item.name === visData.chartAttributeX)
     const attributeIndexY = attributes.findIndex(item => item.name === visData.chartAttributeY)
 
-    const getDataByChartType = () => {
+    const getDataByChartType = useCallback(() => {
         if (visData.chartType === 'pie' && visData.hasToGroup) {
             const ocurrences = countAttributeOcurrences(visibleRows, labelAttributeIndex)
             return ocurrences[0].map((item: any, index: any) => [item, ocurrences[1][index]])
@@ -53,16 +43,17 @@ const Chart: React.FC<ChartPropTypes> = props => {
                 item[attributeIndexY]
             ])
         }
-    }
+    }, [visData, visibleRows, labelAttributeIndex, attributeIndexX, attributeIndexY])
 
-    let data = [
-        [
-            visData.chartLabelAttribute || visData.chartAttributeX,
-            visData.chartAttributeY ?? 'Ocurrences'
-        ],
-        ...getDataByChartType()
-    ]
-    // console.log("chart data", data)
+    let data = useMemo(() => {
+        return [
+            [
+                visData.chartLabelAttribute || visData.chartAttributeX,
+                visData.chartAttributeY ?? 'Ocurrences'
+            ],
+            ...getDataByChartType()
+        ]
+    }, [getDataByChartType, visData])
 
     const getNormalizedChartType = () => {
         switch (visData.chartType) {
@@ -91,8 +82,6 @@ const Chart: React.FC<ChartPropTypes> = props => {
     }
 
     const normalizedChartType = getNormalizedChartType()
-
-    console.log('props', props)
 
     const getChartName = () => {
         if (props.visData.title) {
@@ -126,12 +115,11 @@ const Chart: React.FC<ChartPropTypes> = props => {
         return `Chart ${props.index}`
     }
 
-    const size = 400 - 35 // 35 is scrollbar width
     return (
         <Draggable className={styles.chart} disabled={visMode === 'split'}>
             <GoogleCharts
-                width={size}
-                height={size * 0.75}
+                width={chartSize}
+                height={chartSize * 0.75}
                 chartType={normalizedChartType}
                 loader={<div style={{padding: 30}}>Loading...</div>}
                 data={data}
@@ -148,35 +136,30 @@ const Chart: React.FC<ChartPropTypes> = props => {
                 }}
                 chartEvents={[
                     {
+                        eventName: 'ready',
+                        callback: ({chartWrapper, google}) => {
+                            console.log('chart image base64', chartWrapper.getChart().getImageURI())
+                        }
+                    },
+                    {
                         eventName: 'select',
                         callback: ({chartWrapper, google}) => {
                             const chart = chartWrapper.getChart()
-                            // console.log('chart getSelection', chart.getSelection())
-                            //@ts-ignore
                             const selectedDataRow =
                                 chart.getSelection()?.length > 0
                                     ? data[chart.getSelection()?.[0]?.row + 1]
                                     : null
-                            // console.log('selectedDataRow', selectedDataRow)
-                            // console.log('visibleRows', visibleRows)
-                            // console.log('labelAttributeIndex', labelAttributeIndex)
-                            // console.log('attributeIndexX', attributeIndexX)
                             const highlightIndexes = visibleRows
                                 .map((row: any, index: number) => {
                                     const rowAttributeIndex =
                                         labelAttributeIndex !== -1
                                             ? labelAttributeIndex
                                             : attributeIndexX
-                                    // console.log('selectedDataRow?.[0]', selectedDataRow?.[0])
-                                    // console.log('rowAttributeIndex', rowAttributeIndex)
-                                    // console.log('row[rowAttributeIndex] value', row[rowAttributeIndex])
-                                    // console.log('row[rowAttributeIndex] === selectedDataRow?.[0]', row[rowAttributeIndex], '===', selectedDataRow?.[0], row[rowAttributeIndex] === selectedDataRow?.[0])
                                     return row[rowAttributeIndex] === selectedDataRow?.[0]
                                         ? index
                                         : null
                                 })
                                 .filter((item: any) => item !== null)
-                            // console.log('highlightIndexes', highlightIndexes)
 
                             dispatch(setHighlight(highlightIndexes as number[]))
                         }
