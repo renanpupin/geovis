67f07595e2a74720b1163cd66e3bae9457707928
Renan Pupin <renan.pupin@gmail.com>
2024-08-07 00:11:30 +0200

    ops: add hex converter

--- src/libs/colors.ts
+++ src/libs/colors.ts
@@ -80,3 +80,20 @@ export const generateRandomColors = (size: number): string[] => {
     }
     return colorsArray
 }
+
+export const hex2rgba = (hex: string, alpha: number = 1): string | null => {
+    let parseString = hex
+    if (hex.startsWith('#')) {
+        parseString = hex.slice(1, 7)
+    }
+    if (parseString.length !== 6) {
+        return hex
+    }
+    const r = parseInt(parseString.slice(0, 2), 16)
+    const g = parseInt(parseString.slice(2, 4), 16)
+    const b = parseInt(parseString.slice(4, 6), 16)
+    if (isNaN(r) || isNaN(g) || isNaN(b)) {
+        return hex
+    }
+    return `rgba(${r}, ${g}, ${b}, ${alpha})`
+}
